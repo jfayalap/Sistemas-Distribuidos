0c8df34dfca2c5e694ba905713046b165af706ae
jfayalap <96104899+jfayalap@users.noreply.github.com>
2026-01-19 20:12:06 -0800

    concurrencia.py
    La concurrencia en sistemas distribuidos significa que múltiples procesos o usuarios acceden y modifican recursos compartidos al mismo tiempo, lo que puede generar conflictos si no se controla adecuadamente

--- concurrencia.py
+++ concurrencia.py
@@ -1,3 +1,5 @@
+// código receptor
+
 import cv2
 import socket
 import struct
@@ -37,6 +39,33 @@ while True:
 
     if cv2.waitKey(1) == 27:
         break
+
+//código emisor 
+
+import cv2
+import socket
+import struct
+
+IP_RECEPTOR = '127.0.0.1'
+PUERTO = 9999
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cliente.connect((IP_RECEPTOR, PUERTO))
+
+camara = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = camara.read()
+    if not ret:
+        break
+
+    _, buffer = cv2.imencode('.jpg', frame)
+    data = buffer.tobytes()
+    tamaño = struct.pack("Q", len(data))
+    cliente.sendall(tamaño + data)
+
+camara.release()
+cliente.close()
 
 conn.close()
 cv2.destroyAllWindows()
